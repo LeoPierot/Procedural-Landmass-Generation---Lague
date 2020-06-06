9465ef221bb9aebfc646e90e5c19fec35465ae7d
Léo Piérot <leo.fpierot@gmail.com>
2020-06-06 12:07:09 +0200

    Colour shader
    different colour linked to height levels through the shader

--- Assets/Scripts/Data/TextureData.cs
+++ Assets/Scripts/Data/TextureData.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu()]
 public class TextureData : UpdatableData
 {
+    public Color[] baseColours;
+    [Range(0, 1)]
+    public float[] baseStartHeights;
+    float savedMinHeight;
+    float savedMaxHeight;
+
     public void ApplyToMaterial(Material material)
     {
+        UpdateMeshHeights(material, savedMinHeight, savedMaxHeight);
 
+        material.SetInt("baseColorCount", baseColours.Length);
+        material.SetColorArray("baseColours", baseColours);
+        material.SetFloatArray("baseStartHeights", baseStartHeights);
+    }
+
+    public void UpdateMeshHeights(Material material, float minHeight, float maxHeight)
+    {
+        savedMaxHeight = maxHeight;
+        savedMinHeight = minHeight;
+
+        material.SetFloat("minHeight", minHeight);
+        material.SetFloat("maxHeight", maxHeight);
     }
 }
